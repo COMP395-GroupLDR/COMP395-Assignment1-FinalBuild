7ed44addc920e6e52111f0c5b18c617452cbc6b1
Deadshadow9999 <legendkiller999@hotmail.com>
2023-03-03 23:17:24 -0500

    Added random weather system

---
+++ Assets/Scripts/UI/WeatherStatus.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeatherStatus : MonoBehaviour
+{
+    [SerializeField] private Text weatherStatus;
+
+    private GameObject weatherMaster;
+    private string currentWeather;
+    // Start is called before the first frame update
+    void Start()
+    {
+        weatherMaster = GameObject.Find("WeatherMaster");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentWeather = weatherMaster.GetComponent<Weather_Controller>().en_CurrWeather.ToString();
+        weatherStatus.text = currentWeather;
+    }
+}
